e0ae9bd003a9ea3a88a7d202d0f4b004411c78c7
Truc Tran Trung <truc.trantrung@vml.com>
2018-07-09 17:52:18 +0800

    [ADD] First Screen

---
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.spgroup.spapp.presentation.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.spgroup.spapp.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
