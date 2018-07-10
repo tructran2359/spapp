9d7dba01adac7d710091a5737fd42975806c1247
Truc Tran Trung <truc.trantrung@vml.com>
2018-07-10 11:59:34 +0800

    [ADD] Create BaseActivity class

---
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/BaseActivity.kt
@@ -0,0 +1,9 @@
+package com.spgroup.spapp.presentation.activity
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Base class for activity
+ */
+open class BaseActivity: AppCompatActivity() {
+}
--- SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/HomeActivity.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/HomeActivity.kt
@@ -1,10 +1,9 @@
 package com.spgroup.spapp.presentation.activity
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import com.spgroup.spapp.R
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
