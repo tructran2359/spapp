a09c8692c680d2ec3086f3ea5cfb5ca403502e24
Truc Tran Trung <truc.trantrung@vml.com>
2018-07-10 12:45:59 +0800

    [ADD] First implementation for Log utils

---
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/util/LogUtils.kt
@@ -0,0 +1,34 @@
+package com.spgroup.spapp.presentation.util
+
+import android.util.Log
+import com.spgroup.spapp.BuildConfig.DEBUG
+
+const val TAG_MAX_LENGTH = 23
+const val TAG_PREFIX = "SP_"
+const val TAG_DEFAULT = TAG_PREFIX + "DEFAULT"
+val DEBUGABLE = DEBUG
+
+fun Any.createTag(tag: String): String {
+    var newTag = TAG_PREFIX + tag
+    return if (newTag.length <= TAG_MAX_LENGTH) newTag else newTag.substring(0, TAG_MAX_LENGTH)
+}
+
+fun Any.createMessage(msg: String) = "${this::class.java.simpleName} $msg"
+
+fun Any.doLogD(tag: String = TAG_DEFAULT, msg: String) {
+    if (DEBUGABLE) {
+        Log.d(createTag(tag), createMessage(msg))
+    }
+}
+
+fun Any.doLogI(tag: String = TAG_DEFAULT, msg: String) {
+    if (DEBUGABLE) {
+        Log.i(createTag(tag), createMessage(msg))
+    }
+}
+
+fun Any.doLogE(tag: String = TAG_DEFAULT, msg: String) {
+    if (DEBUGABLE) {
+        Log.e(createTag(tag), createMessage(msg))
+    }
+}
