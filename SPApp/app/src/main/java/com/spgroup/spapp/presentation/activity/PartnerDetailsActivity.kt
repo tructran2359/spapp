bd7845665632ffe41179f566c6105e645b7e6653
Truc Tran Trung <truc.trantrung@vml.com>
2018-07-11 14:32:11 +0800

    VMLSINPOW-78 - Allow to go back to home screen
    + Top left Back button trigger same action with physical back button

--- SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/PartnerDetailsActivity.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/PartnerDetailsActivity.kt
@@ -4,8 +4,13 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import com.spgroup.spapp.R
+import kotlinx.android.synthetic.main.activity_partner_details.*
 
 class PartnerDetailsActivity : BaseActivity() {
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Static
+    ///////////////////////////////////////////////////////////////////////////
 
     companion object {
         fun getLaunchIntent(context: Context): Intent {
@@ -14,8 +19,25 @@ class PartnerDetailsActivity : BaseActivity() {
         }
     }
 
+    ///////////////////////////////////////////////////////////////////////////
+    // Override
+    ///////////////////////////////////////////////////////////////////////////
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_partner_details)
+
+        setupViews()
     }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Other
+    ///////////////////////////////////////////////////////////////////////////
+
+    private fun setupViews() {
+        iv_back.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
 }
