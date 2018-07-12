8da06c7cb09c66c23f25383a2bbb8159f62e0427
Truc Tran Trung <truc.trantrung@vml.com>
2018-07-12 17:57:02 +0800

    VMLSINPOW-86 - Increase  item count by 1 when user click "+" button

--- SPApp/app/src/main/java/com/spgroup/spapp/presentation/view/ServiceItemView.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/view/ServiceItemView.kt
@@ -2,6 +2,7 @@ package com.spgroup.spapp.presentation.view
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.RelativeLayout
 import com.spgroup.spapp.R
 import com.spgroup.spapp.domain.model.ServiceItem
@@ -33,5 +34,21 @@ class ServiceItemView : RelativeLayout {
         LayoutInflater.from(context).inflate(R.layout.layout_service_item, this, true)
         tv_name.setText(serviceItem.name)
         tv_price.setText(serviceItem.price.getFormatedItemPrice(serviceItem.unit))
+        fl_add_btn_container.setOnClickListener {
+            serviceItem.count++
+            onCountUpdate()
+        }
     }
+
+    fun onCountUpdate() {
+        if (serviceItem.count == 0) {
+            tv_count.visibility = View.GONE
+            iv_add.visibility = View.VISIBLE
+        } else {
+            tv_count.visibility = View.VISIBLE
+            tv_count.setText(serviceItem.count.toString())
+            iv_add.visibility = View.GONE
+        }
+    }
+
 }
