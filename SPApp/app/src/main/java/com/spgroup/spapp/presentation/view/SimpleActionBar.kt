d8805bada2d10a9a06555a55140fd83bec374988
Truc Tran Trung <truc.trantrung@vml.com>
2018-07-18 16:16:19 +0700

    VLMSINPOW-148 - Create custom view: SimpleActionBar

---
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/view/SimpleActionBar.kt
@@ -0,0 +1,46 @@
+package com.spgroup.spapp.presentation.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.RelativeLayout
+import com.spgroup.spapp.R
+import kotlinx.android.synthetic.main.layout_simple_action_bar.view.*
+
+class SimpleActionBar: RelativeLayout {
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Constructor
+    ///////////////////////////////////////////////////////////////////////////
+
+    constructor(context: Context) : super(context) {
+        initViews(context)
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet): super(context, attributeSet) {
+        initViews(context)
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet, defStyle: Int): super(context, attributeSet, defStyle) {
+        initViews(context)
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Other
+    ///////////////////////////////////////////////////////////////////////////
+
+    private fun initViews(context: Context) {
+        LayoutInflater.from(context).inflate(R.layout.layout_simple_action_bar, this, true)
+    }
+
+    fun setOnBackPress(action: () -> Unit) {
+        iv_back.setOnClickListener {
+            action()
+        }
+    }
+
+    fun setTitle(resId: Int) {
+        tv_title.setText(resId)
+    }
+
+}
