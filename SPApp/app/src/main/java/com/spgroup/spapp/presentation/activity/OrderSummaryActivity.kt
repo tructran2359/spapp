c1df040247affc82bbd300a17aead952810d13a8
Truc Tran Trung <truc.trantrung@vml.com>
2018-07-18 13:08:07 +0700

    VMLSINPOW-145 - Dropdown list for Preferred Time of Contact

--- SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/OrderSummaryActivity.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/OrderSummaryActivity.kt
@@ -3,6 +3,7 @@ package com.spgroup.spapp.presentation.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.spgroup.spapp.R
 import kotlinx.android.synthetic.main.activity_order_summary.*
 
@@ -37,5 +38,11 @@ class OrderSummaryActivity : BaseActivity() {
         iv_back.setOnClickListener {
             onBackPressed()
         }
+
+        val list = listOf("11AM - 12PM", "12PM - 2PM", "2PM - 4PM")
+        val adapter = ArrayAdapter(this, R.layout.layout_preferred_time, list)
+        adapter.setDropDownViewResource(R.layout.layout_preferred_time_dropdown)
+
+        spinner_preferred_time.adapter = adapter
     }
 }
