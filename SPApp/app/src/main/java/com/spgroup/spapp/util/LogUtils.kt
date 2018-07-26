80b0452dd4e615f7d1408aff84198d7f7a8aa515
Truc Tran Trung <truc.trantrung@vml.com>
2018-07-26 17:44:35 +0700

    [FIX] typo

--- SPApp/app/src/main/java/com/spgroup/spapp/util/LogUtils.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/util/LogUtils.kt
@@ -1,12 +1,12 @@
 package com.spgroup.spapp.util
 
 import android.util.Log
-import com.spgroup.spapp.BuildConfig.DEBUG
+import com.spgroup.spapp.BuildConfig
 
 const val TAG_MAX_LENGTH = 23
 const val TAG_PREFIX = "SP_"
 const val TAG_DEFAULT = "DEFAULT"
-val DEBUGABLE = DEBUG
+val DEBUGGABLE = BuildConfig.DEBUG
 
 fun Any.createTag(tag: String): String {
     var newTag = TAG_PREFIX + tag
@@ -16,19 +16,19 @@ fun Any.createTag(tag: String): String {
 fun Any.createMessage(msg: String) = "${this::class.java.simpleName} $msg"
 
 fun Any.doLogD(tag: String = TAG_DEFAULT, msg: String) {
-    if (DEBUGABLE) {
+    if (DEBUGGABLE) {
         Log.d(createTag(tag), createMessage(msg))
     }
 }
 
 fun Any.doLogI(tag: String = TAG_DEFAULT, msg: String) {
-    if (DEBUGABLE) {
+    if (DEBUGGABLE) {
         Log.i(createTag(tag), createMessage(msg))
     }
 }
 
 fun Any.doLogE(tag: String = TAG_DEFAULT, msg: String) {
-    if (DEBUGABLE) {
+    if (DEBUGGABLE) {
         Log.e(createTag(tag), createMessage(msg))
     }
 }
