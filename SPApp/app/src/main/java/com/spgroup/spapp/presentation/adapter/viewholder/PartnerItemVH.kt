88f7fe57486104475d66893e084d4eca80d27f59
Truc Tran Trung <truc.trantrung@vml.com>
2018-08-09 17:48:47 +0700

    VMLSINPOW-366 - Load partner logo from url

--- SPApp/app/src/main/java/com/spgroup/spapp/presentation/adapter/viewholder/PartnerItemVH.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/adapter/viewholder/PartnerItemVH.kt
@@ -2,9 +2,10 @@ package com.spgroup.spapp.presentation.adapter.viewholder
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import com.bumptech.glide.Glide
 import com.spgroup.spapp.domain.model.Partner
 import com.spgroup.spapp.domain.model.PartnersListingItem
-import com.spgroup.spapp.util.extension.formatPriceWithUnit
+import com.spgroup.spapp.util.extension.toFullImgUrl
 import kotlinx.android.synthetic.main.layout_partner_item.view.*
 
 class PartnerItemVH(
@@ -16,7 +17,11 @@ class PartnerItemVH(
         view.run {
             iv_sponsored.visibility = if (partner.isSponsored) View.VISIBLE else View.GONE
             tv_name.text = partner.name
-            tv_price.text = partner.price.formatPriceWithUnit(partner.unit)
+            tv_price.text = partner.priceDescription
+            Glide.with(context)
+                    .load(partner.imgUrl.toFullImgUrl())
+                    .into(iv_logo)
+
             setOnClickListener {
                 itemClickListener.invoke(view, partner, adapterPosition)
             }
