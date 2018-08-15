f9c71e9705977aabf73c59f049cc8d8edd3f44cb
Truc Tran Trung <truc.trantrung@vml.com>
2018-08-15 12:22:44 +0700

    VMSINPOW-364 - Featured Partner on Home Screen

---
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/adapter/viewholder/HomeMerchantVH.kt
@@ -0,0 +1,23 @@
+package com.spgroup.spapp.presentation.adapter.viewholder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.spgroup.spapp.domain.model.TopLevelFeaturedPartner
+import com.spgroup.spapp.presentation.adapter.HomeMerchantAdapter
+import com.spgroup.spapp.util.extension.loadImage
+import com.spgroup.spapp.util.extension.toFullImgUrl
+import kotlinx.android.synthetic.main.layout_merchant.view.*
+
+class HomeMerchantVH(itemView: View, private val listener: HomeMerchantAdapter.OnMerchantClickListener): RecyclerView.ViewHolder(itemView) {
+
+    fun bind(partner: TopLevelFeaturedPartner) {
+        itemView.run {
+            tv_category.text = partner.category
+            tv_partner_name.text = partner.name
+            iv_logo.loadImage(partner.logoUrl.toFullImgUrl())
+            setOnClickListener {
+                listener.onMerchantClick(adapterPosition)
+            }
+        }
+    }
+}
