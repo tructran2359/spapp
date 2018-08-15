b16ac9e49594cf0d1abf2565465d72dfd77b313b
Truc Tran Trung <truc.trantrung@vml.com>
2018-08-15 12:03:00 +0700

    VMLSINPOW-362 - Home Featured Promotions

--- SPApp/app/src/main/java/com/spgroup/spapp/presentation/adapter/HomePromotionAdapter.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/adapter/HomePromotionAdapter.kt
@@ -1,30 +1,30 @@
 package com.spgroup.spapp.presentation.adapter
 
-import android.content.Context
 import android.support.v7.widget.RecyclerView
-import android.view.View
 import android.view.ViewGroup
 import com.spgroup.spapp.R
+import com.spgroup.spapp.domain.model.TopLevelPromotion
+import com.spgroup.spapp.presentation.adapter.viewholder.HomePromotionVH
 import com.spgroup.spapp.util.extension.getDimensionPixelSize
 import com.spgroup.spapp.util.extension.inflate
 import kotlinx.android.synthetic.main.layout_top_level_promotion.view.*
 
 class HomePromotionAdapter(
-        val context: Context,
-        val screenWidth: Int
-): RecyclerView.Adapter<HomePromotionAdapter.TopCatePromotionVH>() {
+        private val screenWidth: Int,
+        private var mListener: OnPromotionClickListener
+): RecyclerView.Adapter<HomePromotionVH>() {
 
     ///////////////////////////////////////////////////////////////////////////
     // Property
     ///////////////////////////////////////////////////////////////////////////
 
-    private var mListener: OnPromotionClickListener? = null
+    private val mData = mutableListOf<TopLevelPromotion>()
 
     ///////////////////////////////////////////////////////////////////////////
     // Override
     ///////////////////////////////////////////////////////////////////////////
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopCatePromotionVH {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomePromotionVH {
         val view = parent.inflate(R.layout.layout_top_level_promotion, false)
         with(view) {
             iv_image.setRatio(16, 9, true)
@@ -34,12 +34,13 @@ class HomePromotionAdapter(
             layoutParams.width = screenWidth - (2 * padding) + (2 * shadow)
             view_root.layoutParams = layoutParams
         }
-        return TopCatePromotionVH(view)
+        return HomePromotionVH(view, mListener)
     }
 
-    override fun getItemCount() = 10
+    override fun getItemCount() = mData.size
 
-    override fun onBindViewHolder(viewHolder: TopCatePromotionVH, position: Int) {
+    override fun onBindViewHolder(viewHolder: HomePromotionVH, position: Int) {
+        viewHolder.bind(mData[position])
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -50,17 +51,10 @@ class HomePromotionAdapter(
         mListener = listener
     }
 
-    ///////////////////////////////////////////////////////////////////////////
-    // ViewHolder
-    ///////////////////////////////////////////////////////////////////////////
-
-    inner class TopCatePromotionVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        init {
-            itemView.setOnClickListener {
-                mListener?.onPromotionClick(adapterPosition)
-            }
-        }
+    fun setData(data: List<TopLevelPromotion>) {
+        mData.clear()
+        mData.addAll(data)
+        notifyDataSetChanged()
     }
 
     ///////////////////////////////////////////////////////////////////////////
--- SPApp/app/src/main/java/com/spgroup/spapp/presentation/viewmodel/HomeViewModel.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/viewmodel/HomeViewModel.kt
@@ -2,6 +2,8 @@ package com.spgroup.spapp.presentation.viewmodel
 
 import android.arch.lifecycle.MutableLiveData
 import com.spgroup.spapp.domain.model.TopLevelCategory
+import com.spgroup.spapp.domain.model.TopLevelFeaturedPartner
+import com.spgroup.spapp.domain.model.TopLevelPromotion
 import com.spgroup.spapp.manager.AppDataCache
 
 class HomeViewModel(
@@ -13,9 +15,13 @@ class HomeViewModel(
     ///////////////////////////////////////////////////////////////////////////
 
     val listTopLevelCate = MutableLiveData<List<TopLevelCategory>>()
+    val listTopLevelPromotion = MutableLiveData<List<TopLevelPromotion>>()
+    val listTopLevelPartner = MutableLiveData<List<TopLevelFeaturedPartner>>()
 
-    fun getTopLevelCategory() {
+    fun getInitData() {
         listTopLevelCate.value = appDataCache.getTopLevelCategories()
+        listTopLevelPromotion.value = appDataCache.getTopLevelPromotions()
+        listTopLevelPartner.value = appDataCache.getTopLevelPartners()
     }
 
     fun getCategoryByIndex(index: Int) = listTopLevelCate.value?.get(index)
