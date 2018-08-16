6347497b0b1c1fc0e084fea9d9e7664c6078da04
Truc Tran Trung <truc.trantrung@vml.com>
2018-08-17 02:27:15 +0700

    VMLSINPOW-444 - PartnerInfo screen UI

--- SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/PartnerInformationActivity.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/PartnerInformationActivity.kt
@@ -6,15 +6,21 @@ import android.os.Bundle
 import com.spgroup.spapp.R
 import com.spgroup.spapp.presentation.view.IndicatorTextView
 import kotlinx.android.synthetic.main.activity_partner_information.*
+import java.io.Serializable
 
 class PartnerInformationActivity : BaseActivity() {
 
     companion object {
-        fun getLaunchIntent(context: Context): Intent {
+
+        const val EXTRA_PARTNER_INFO = "EXTRA_PARTNER_INFO"
+        fun getLaunchIntent(context: Context, partnerInfo: PartnerInfo): Intent {
             val intent = Intent(context, PartnerInformationActivity::class.java)
+            intent.putExtra(EXTRA_PARTNER_INFO, partnerInfo)
             return intent
         }
     }
+
+    private lateinit var mData: PartnerInfo
 
     ///////////////////////////////////////////////////////////////////////////
     // Override
@@ -24,6 +30,8 @@ class PartnerInformationActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_partner_information)
 
+        mData = intent.getSerializableExtra(EXTRA_PARTNER_INFO) as PartnerInfo
+
         initViews()
     }
 
@@ -32,13 +40,14 @@ class PartnerInformationActivity : BaseActivity() {
     ///////////////////////////////////////////////////////////////////////////
 
     private fun initViews() {
-        val list = listOf(
-                "Dry Cleaning",
-                "Curtain Cleaning",
-                "Enjoy free collection & delivery over $30",
-                "Special request available")
 
-        for (str in list) {
+        tv_name.text = mData.name
+        tv_description.text = mData.desc
+        tv_phone.text = mData.phone
+        tv_uen.text = mData.uen
+        tv_nea.text = mData.nea
+
+        for (str in mData.offers) {
             val view = IndicatorTextView(this, str)
             ll_service_container.addView(view)
         }
@@ -47,4 +56,13 @@ class PartnerInformationActivity : BaseActivity() {
             onBackPressed()
         }
     }
+
+    data class PartnerInfo(
+            val name: String,
+            val desc: String,
+            val offers: List<String>,
+            val phone: String,
+            val uen: String,
+            val nea: String
+    ): Serializable
 }
