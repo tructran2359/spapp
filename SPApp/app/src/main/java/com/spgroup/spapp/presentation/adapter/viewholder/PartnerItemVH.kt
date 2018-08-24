5523067a13ff7c852a87b5580f54b8399e14498d
Truc Tran Trung <truc.trantrung@vml.com>
2018-08-24 19:59:13 +0700

    [FIX] Use textview to show highlight in Partner Listing

--- SPApp/app/src/main/java/com/spgroup/spapp/presentation/adapter/viewholder/PartnerItemVH.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/adapter/viewholder/PartnerItemVH.kt
@@ -2,6 +2,7 @@ package com.spgroup.spapp.presentation.adapter.viewholder
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import androidx.core.view.isGone
 import com.bumptech.glide.Glide
 import com.spgroup.spapp.domain.model.Partner
 import com.spgroup.spapp.domain.model.PartnersListingItem
@@ -15,7 +16,8 @@ class PartnerItemVH(
 
     fun bind(partner: Partner) {
         view.run {
-            iv_sponsored.visibility = if (partner.highlight.isEmpty()) View.GONE else View.VISIBLE
+            tv_highlight.isGone = partner.highlight.isEmpty()
+            tv_highlight.text = partner.highlight
             tv_name.text = partner.name
             tv_price.text = partner.priceDescription
             Glide.with(context)
