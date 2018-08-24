95b1ece7700fa735bd79b5938f77c114c05f4df5
Truc Tran Trung <truc.trantrung@vml.com>
2018-08-24 15:25:03 +0700

    VMLSINPOW-648 - Create Model to submit order request

---
+++ SPApp/app/src/main/java/com/spgroup/spapp/domain/model/ContactInfo.kt
@@ -0,0 +1,11 @@
+package com.spgroup.spapp.domain.model
+
+data class ContactInfo(
+        val name: String,
+        val email: String,
+        val contactNo: String,
+        val address: List<String>,
+        val prefContactTime: String,
+        val postalCode: String,
+        val notes: String
+)
