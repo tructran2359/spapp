b717f2fd51d2ccecc43ac626fa505df1eb432ac6
Truc Tran Trung <truc.trantrung@vml.com>
2018-09-04 18:59:31 +0700

    VMLSINPOW-700 - Reuse layout file

--- SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/NoInternetActivity.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/NoInternetActivity.kt
@@ -6,7 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import com.spgroup.spapp.R
 import com.spgroup.spapp.util.extension.isOnline
-import kotlinx.android.synthetic.main.activity_no_internet.*
+import kotlinx.android.synthetic.main.activity_error.*
 import org.jetbrains.anko.longToast
 
 class NoInternetActivity: BaseActivity() {
@@ -22,7 +22,7 @@ class NoInternetActivity: BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_no_internet)
+        setContentView(R.layout.activity_error)
 
         setUpViews()
     }
