f81318ceca2b3a28dc01f735eef231ae4c7c8838
Truc Tran Trung <truc.trantrung@vml.com>
2018-09-06 13:45:53 +0700

    VMLSINPOW-707 - SplashActivity: when api error, move to Error Page. Click RETRY to load again

--- SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/ApiErrorActivity.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/ApiErrorActivity.kt
@@ -1,11 +1,15 @@
 package com.spgroup.spapp.presentation.activity
 
-import android.app.Activity
+import android.arch.lifecycle.Observer
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.core.view.isGone
 import com.spgroup.spapp.R
+import com.spgroup.spapp.presentation.viewmodel.SplashViewModel
+import com.spgroup.spapp.presentation.viewmodel.ViewModelFactory
+import com.spgroup.spapp.util.doLogE
+import com.spgroup.spapp.util.extension.obtainViewModel
 import com.spgroup.spapp.util.extension.setLayoutParamsSizeFromDimens
 import kotlinx.android.synthetic.main.activity_error.*
 
@@ -15,12 +19,50 @@ class ApiErrorActivity: BaseErrorActivity() {
 
         fun getLaunchIntent(context: Context) = Intent(context, ApiErrorActivity::class.java)
 
+        fun getLaunchIntentForSplash(context: Context): Intent {
+            val intent = Intent(context, ApiErrorActivity::class.java)
+            intent.putExtra(EXTRA_FOR_SPLASH, true)
+            return intent
+        }
     }
+
+    private var mForSplash = false
+    private var mViewModel: SplashViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        mForSplash = intent.getBooleanExtra(EXTRA_FOR_SPLASH, false)
+
+        if (mForSplash) {
+            setUpViewModel()
+        }
         setUpViews()
+    }
+
+    private fun setUpViewModel() {
+        mViewModel = obtainViewModel(SplashViewModel::class.java, ViewModelFactory.getInstance())
+        subscibeUI()
+    }
+
+    private fun subscibeUI() {
+        mViewModel?.run {
+            isSuccess.observe(this@ApiErrorActivity, Observer {
+                updateEnabled(true)
+                it?.let { success ->
+                    if (success) {
+                        val intent = mViewModel?.getIntentToNextActivity(this@ApiErrorActivity)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            })
+
+            error.observe(this@ApiErrorActivity, Observer {
+                doLogE("Error", "Error $it with message: ${it?.message}")
+                updateEnabled(true)
+            })
+        }
     }
 
     private fun setUpViews() {
@@ -31,15 +73,21 @@ class ApiErrorActivity: BaseErrorActivity() {
         iv_error_icon.setImageResource(R.drawable.error_api)
         tv_title.setText(R.string.api_error_title)
         tv_description.setText(R.string.api_error_desc)
-        tv_refresh.setText(R.string.back)
         tv_back.isGone = true
+
+        tv_refresh.setText(if (mForSplash) R.string.retry else R.string.back)
         tv_refresh.setOnClickListener {
-            onBackPressed()
+            if (mForSplash) {
+                updateEnabled(false)
+                mViewModel?.getInitialData()
+            } else {
+                onBackPressed()
+            }
         }
     }
 
-    private fun goBackWithResultOk() {
-        setResult(Activity.RESULT_OK)
-        finish()
+    private fun updateEnabled(enabled: Boolean) {
+        tv_refresh.isEnabled = enabled
+        tv_refresh.setBackgroundResource(if (enabled) R.drawable.selector_btn_main else R.drawable.bg_rec_rounded_main_disabled)
     }
 }
