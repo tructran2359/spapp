b71434755eb1dea4e8614dc3051e5f9432e5b76e
Truc Tran Trung <truc.trantrung@vml.com>
2018-09-10 16:19:02 +0700

    VMLSINPOW-759 - Dialog to show when minimum order is not satisfied
    + Refactor BaseDialog class

--- SPApp/app/src/main/java/com/spgroup/spapp/presentation/fragment/LoadingDialog.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/fragment/LoadingDialog.kt
@@ -1,20 +1,8 @@
 package com.spgroup.spapp.presentation.fragment
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.spgroup.spapp.R
 
 class LoadingDialog: BaseDialog() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.dialog_loading, container, false)
-        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        dialog.setCanceledOnTouchOutside(false)
-
-        return view
-    }
+    override fun getLayoutResId() = R.layout.dialog_loading
 }
