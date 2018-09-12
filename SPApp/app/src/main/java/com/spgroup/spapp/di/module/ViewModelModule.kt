075f4ff7c0ec76342e71452ce1e01a4c5c957a6e
Truc Tran Trung <truc.trantrung@vml.com>
2018-09-12 21:06:29 +0700

    [FIX] typo

--- SPApp/app/src/main/java/com/spgroup/spapp/di/module/ViewModelModule.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/di/module/ViewModelModule.kt
@@ -13,7 +13,7 @@ import dagger.multibindings.IntoMap
 abstract class ViewModelModule {
 
     @Binds
-    abstract fun provideViewModelFactory(vmf: AppViewModelFactory): ViewModelProvider.Factory
+    abstract fun bindViewModelFactory(vmf: AppViewModelFactory): ViewModelProvider.Factory
 
     @Binds
     @IntoMap
