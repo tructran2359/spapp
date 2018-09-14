aa0cb7bda80ea601809d56ec5c97ae3e3c8047d0
Truc Tran Trung <truc.trantrung@vml.com>
2018-09-14 16:40:11 +0700

    [ADD] Animation

--- SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/SplashActivity.kt
+++ SPApp/app/src/main/java/com/spgroup/spapp/presentation/activity/SplashActivity.kt
@@ -5,12 +5,14 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.core.view.isGone
 import com.spgroup.spapp.R
 import com.spgroup.spapp.presentation.viewmodel.SplashViewModel
 import com.spgroup.spapp.util.doLogE
-import com.spgroup.spapp.util.extension.appInstance
-import com.spgroup.spapp.util.extension.isOnline
-import com.spgroup.spapp.util.extension.obtainViewModel
+import com.spgroup.spapp.util.extension.*
+import kotlinx.android.synthetic.main.activity_splash.*
 import javax.inject.Inject
 
 class SplashActivity : BaseActivity() {
@@ -23,6 +25,8 @@ class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+//        testAnim()
 
         appInstance.appComponent.inject(this)
         splashViewModel = obtainViewModel(SplashViewModel::class.java, vmFactory)
@@ -64,13 +68,82 @@ class SplashActivity : BaseActivity() {
 
     private fun onDownloadDataSuccess() {
         val intent = splashViewModel.getIntentToNextActivity(this)
-        startActivity(intent)
-        finish()
+        startActivityWithAnimation(intent)
     }
 
     private fun onDownloadDataFailed(throwable: Throwable?) {
         doLogE("Error", "Error $throwable with message: ${throwable?.message ?: "null"}")
         startActivity(ApiErrorActivity.getLaunchIntentForSplash(this))
         finish()
+    }
+
+    private fun startActivityWithAnimation(intent: Intent) {
+        val disappearAnim = AnimationUtils.loadAnimation(this, R.anim.anim_splash_icon_disappear)
+        disappearAnim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                iv_main.isGone = true
+                startActivity(intent)
+                overridePendingTransition(R.anim.anim_fade_in, R.anim.anim_fade_out)
+                finish()
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+            }
+        })
+
+        val scaleUpAnim = AnimationUtils.loadAnimation(this, R.anim.anim_splash_icon_scale_up)
+        scaleUpAnim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                val iconSizeScaledUp = (getDimensionPixelSize(R.dimen.splash_image_size) * 1.2).toInt()
+                iv_main.setLayoutParamsSize(iconSizeScaledUp, iconSizeScaledUp)
+                iv_main.startAnimation(disappearAnim)
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+            }
+        })
+
+        iv_main.startAnimation(scaleUpAnim)
+    }
+
+
+
+    private fun testAnim() {
+        val disappearAnim = AnimationUtils.loadAnimation(this, R.anim.anim_splash_icon_disappear)
+        val scaleUpAnim = AnimationUtils.loadAnimation(this, R.anim.anim_splash_icon_scale_up)
+
+        disappearAnim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                iv_main.startAnimation(scaleUpAnim)
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+            }
+        })
+
+        scaleUpAnim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                val iconSizeScaledUp = (getDimensionPixelSize(R.dimen.splash_image_size) * 1.2).toInt()
+                iv_main.setLayoutParamsSize(iconSizeScaledUp, iconSizeScaledUp)
+                iv_main.startAnimation(disappearAnim)
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+            }
+        })
+
+        iv_main.startAnimation(scaleUpAnim)
     }
 }
